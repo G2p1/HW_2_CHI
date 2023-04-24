4b6036ec7fe0f718b75f9c39f80b82091b2fd730
G2p1 <andrii.tiuitunnyk@gmail.com>
2023-04-24 17:09:31 +0300

    Task 1: add class StringManupulator with function that calculate sum numbrs in string and find maximum number in string

--- HW_2/Program.cs
+++ HW_2/Program.cs
@@ -6,10 +6,39 @@ using System.Threading.Tasks;
 
 namespace HW_2
 {
+    class StringManipulator
+    {
+        public void operationNumberInString(string inputString)
+        {
+            string[] tokens = inputString.Split(' ', ',', ';');
+            int sum = 0;
+            int maxInString;
+            int.TryParse(string.Join("", tokens[0].Where(c => char.IsDigit(c))), out maxInString);
+            sum += maxInString;
+
+            for (int token = 1; token < tokens.Length;token++)
+            {
+                int number;
+                int.TryParse(string.Join("", tokens[token].Where(c => char.IsDigit(c))), out number);
+                
+                if (number > maxInString)
+                {
+                    maxInString = number;
+                }
+                
+                sum += number;
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine(maxInString);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            StringManipulator sManipulator = new StringManipulator();
+
+            sManipulator.operationNumberInString(Console.ReadLine());
         }
     }
 }
