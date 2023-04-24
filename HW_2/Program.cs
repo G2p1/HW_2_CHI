5061b298f1ca5d748ea19feca3aefa6ad28203ff
G2p1 <andrii.tiuitunnyk@gmail.com>
2023-04-25 00:27:48 +0300

    Task3: add class AArrayManipulator with method that find maximum pages in book

--- HW_2/Program.cs
+++ HW_2/Program.cs
@@ -72,15 +72,43 @@ namespace HW_2
             Console.WriteLine(maxIndex - startIndex);
         }
     }
+
+    class ArrayManipulator
+    {
+        public void bookPages()
+        {
+            Random rnd = new Random();
+            int[] pageInBook = new int[100];
+
+            for(int i = 0; i < 100; i++)
+            {
+                pageInBook[i] = rnd.Next(1, 1000);
+            }
+
+            int maxPage = pageInBook[0];
+            for(int i = 1; i<100; i++)
+            {
+                if(maxPage < pageInBook[i])
+                {
+                    maxPage = pageInBook[i];
+                }
+            }
+            Console.WriteLine(maxPage);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             StringManipulator sManipulator = new StringManipulator();
+            ArrayManipulator aManipulator = new ArrayManipulator();
 
-            sManipulator.operationNumberInString(Console.ReadLine());
+         //   sManipulator.operationNumberInString(Console.ReadLine());
+        
+//            sManipulator.indexMaxNumberInString(Console.ReadLine().ToCharArray());
 
-            sManipulator.indexMaxNumberInString(Console.ReadLine().ToCharArray());
+            aManipulator.bookPages();
         }
     }
 }
